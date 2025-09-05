8cbad5a48bb4a67311c07ce3368644899ddaf5ac
PavelAC <pavelandreicalin@gmail.com>
2025-09-05 14:10:04 +0300

    add footer component with template and styles

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+
+@Component({
+  selector: 'app-footer',
+  imports: [MaterialModule],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {}
