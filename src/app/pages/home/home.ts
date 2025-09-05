84f347c5bc4acf4dfb2624d6855281f0baea55f3
PavelAC <pavelandreicalin@gmail.com>
2025-09-05 14:11:31 +0300

    add home component with template and styles

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+
+@Component({
+  selector: 'app-home',
+  imports: [MaterialModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {}
