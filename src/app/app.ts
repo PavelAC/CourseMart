f9654d4d73a4585fabaf015c909e292827cee35b
PavelAC <pavelandreicalin@gmail.com>
2025-09-18 14:01:30 +0300

    add-background-animations-component-and-integrate-into-app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { BackgroundAnimationsComponent } from "./components/background-animations.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, BackgroundAnimationsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
